308f12db6ef846ea6fe24753fa27a14881e6d551
Samuel Estabrook <samuel.estabrook@gmail.com>
2016-03-28 20:10:52 -0400

    removal of unnecessary files
    removing some development files that are distracting

--- download_dc_osm.sh
+++
@@ -1,4 +0,0 @@
-wget http://download.geofabrik.de/north-america/us-south-latest.osm.pbf
-brew install osmosis
-brew cleanup -s
-osmosis --rb us-south-latest.osm.pbf --bounding-box left=-77.50 right=-76.50 top=39.25 bottom=38.50 --wb dc-area.osm.pbf
